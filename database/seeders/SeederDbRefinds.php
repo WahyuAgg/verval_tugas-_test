6bdbc08b872b3b8886aef718ac9975c13597bb58
WahyuAgg <wahyuagungbarokah@mail.ugm.ac.id>
2024-11-21 10:02:52 +0700

    enable seed

--- database/seeders/SeederDbRefinds.php
+++ database/seeders/SeederDbRefinds.php
@@ -25,33 +25,33 @@ class SeederDbRefinds extends Seeder
 
         // //// RefindsUser & Alamat
         RefindsUser::factory(30)->create();
-        // Alamat::factory(120)->create();
+        Alamat::factory(120)->create();
 
-        // //// produk + gambar produk
-        // $jumlahProduk = 500;
-        // Produk::factory()->count($jumlahProduk)->create()
-        // ->each(function ($produk) {
-        //     // Membuat 4 gambar untuk setiap produk
-        //     GambarProduk::factory()->count(4)->create([
-        //         'id_produk' => $produk->id_produk, // Menghubungkan gambar dengan produk
-        //     ]);
-        // });
+        //// produk + gambar produk
+        $jumlahProduk = 500;
+        Produk::factory()->count($jumlahProduk)->create()
+        ->each(function ($produk) {
+            // Membuat 4 gambar untuk setiap produk
+            GambarProduk::factory()->count(4)->create([
+                'id_produk' => $produk->id_produk, // Menghubungkan gambar dengan produk
+            ]);
+        });
 
 
 
-        // //// traksaksi + ulasan
-        // $jumlahTransaksi = 120;
-        // Transaksi::factory()
-        // ->count($jumlahTransaksi)
-        // ->create()
-        // ->each(function ($transaksi) {
-        //     // Memberikan kemungkinan 50% untuk membuat ulasan
-        //     if (fake()->boolean(70)) { // 70% kemungkinan untuk memiliki ulasan
-        //         Ulasan::factory()->create([
-        //             'id_transaksi' => $transaksi->id_transaksi, // Menghubungkan ulasan dengan transaksi
-        //         ]);
-        //     }
-        // });
+        //// traksaksi + ulasan
+        $jumlahTransaksi = 120;
+        Transaksi::factory()
+        ->count($jumlahTransaksi)
+        ->create()
+        ->each(function ($transaksi) {
+            // Memberikan kemungkinan 50% untuk membuat ulasan
+            if (fake()->boolean(60)) { // 60% kemungkinan untuk memiliki ulasan
+                Ulasan::factory()->create([
+                    'id_transaksi' => $transaksi->id_transaksi, // Menghubungkan ulasan dengan transaksi
+                ]);
+            }
+        });
 
 
 

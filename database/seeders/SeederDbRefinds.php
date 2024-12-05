15ccead61f31f9e6ee48f43d23cc927527d247c9
WahyuAgg <wahyuagungbarokah@mail.ugm.ac.id>
2024-12-05 18:11:42 +0700

    database config

--- database/seeders/SeederDbRefinds.php
+++ database/seeders/SeederDbRefinds.php
@@ -24,18 +24,18 @@ class SeederDbRefinds extends Seeder
         // SubKategori::factory(0)->create();
 
         /// RefindsUser & Alamat
-        // RefindsUser::factory(30)->create();
-        // Alamat::factory(120)->create();
+        RefindsUser::factory(30)->create();
+        Alamat::factory(120)->create();
 
-        // /// produk + gambar produk
-        // $jumlahProduk = 500;
-        // Produk::factory()->count($jumlahProduk)->create()
-        // ->each(function ($produk) {
-        //     // Membuat 4 gambar untuk setiap produk
-        //     GambarProduk::factory()->count(4)->create([
-        //         'id_produk' => $produk->id_produk, // Menghubungkan gambar dengan produk
-        //     ]);
-        // });
+        /// produk + gambar produk
+        $jumlahProduk = 500;
+        Produk::factory()->count($jumlahProduk)->create()
+        ->each(function ($produk) {
+            // Membuat 4 gambar untuk setiap produk
+            GambarProduk::factory()->count(4)->create([
+                'id_produk' => $produk->id_produk, // Menghubungkan gambar dengan produk
+            ]);
+        });
 
 
 

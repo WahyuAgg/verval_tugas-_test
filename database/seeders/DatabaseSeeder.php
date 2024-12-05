a07d636697c150dcb729093b4001608169e3ebdc
WahyuAgg <wahyuagungbarokah@mail.ugm.ac.id>
2024-12-05 20:29:08 +0700

    migration opt

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\DB;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,9 +15,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Menjalankan seeders lainnya
         $this->call([
             RefindsDefault::class,
             SeederDbRefinds::class,
         ]);
+
+        // Eksekusi query untuk memperbarui transaksi
+        DB::statement("
+            UPDATE transaksi t
+            JOIN (
+                SELECT id_transaksi
+                FROM transaksi
+                ORDER BY RAND()
+                LIMIT 40
+            ) AS random_transaksi ON t.id_transaksi = random_transaksi.id_transaksi
+            SET t.id_user_pembeli = FLOOR(1 + (RAND() * 2)); -- 1 atau 2
+        ");
+
+        // Eksekusi query untuk memperbarui produk
+        DB::statement("
+            UPDATE produk p
+            JOIN (
+                SELECT id_produk
+                FROM produk
+                ORDER BY RAND()
+                LIMIT 50
+            ) AS random_produk ON p.id_produk = random_produk.id_produk
+            SET p.id_user = FLOOR(1 + (RAND() * 2)); -- 1 atau 2
+        ");
     }
 }
+
